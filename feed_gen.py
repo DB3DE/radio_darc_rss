344dfffe6ca3a65655b9fd699c3e4ec3dfcee6e6
Birk Bremer <birk.bremer@5pm.de>
2020-02-13 21:36:20 +0100

    Added class and webside read

--- feed_gen.py
+++ feed_gen.py
@@ -4,12 +4,48 @@ import datetime
 import yaml
 import os
 import sys
+import re
+import datetime
+
+from lib.rfeed import *
 
 sPATH_CONFIG_FILE=r"~/.radio_darc_feedgen.yaml"
+config = None
+PodElements = list()
+
+class PodElement():
+    """ Represents an object that can be host to other extensions.
+    """
+    def __init__(self, sRawline):
+        self.__sRasline = sRawline
+
+        self.title = None   #Title of this podcast issue
+        self.filename = None
+        self.url = None     #url of ths podcast issue
+        self.description = None
+        self.pubDate = None
+
+    def ExtractData(self):
+        #print(self.__sRasline)
+        rem = re.search('.*<a href=\"(.*.mp3).*/(.*)/(.*)</a>.*', self.__sRasline)
+
+        #filename and url
+        self.filename = rem.group(1).strip()
+        self.url = config["url"] + self.filename
+
+        # extract the date
+        date_str = rem.group(3).strip()
+        date_dy, date_mon, date_yr = date_str.split(".")
+        self.pubDate = datetime.datetime(int(date_yr), int(date_mon), int(date_dy))
+
+        transmission = rem.group(2).strip()
+        self.title = "Radio DARC "+transmission
+        self.description = "Radio DARC " + transmission + " from " + date_str
 
 
-def read_config():
-# reads the config file and returns a dictionary
+
+def read_config():  # reads the config file and set the global variable
+    global config
     sAbs_ConfigFilePath=os.path.abspath(os.path.expanduser(sPATH_CONFIG_FILE))
     if not os.path.exists(sAbs_ConfigFilePath):
         print("Could not find config file %s"%(sAbs_ConfigFilePath))
@@ -17,11 +53,12 @@ def read_config():
 
     with open(sAbs_ConfigFilePath, 'r') as stream:
         try:
-            return yaml.safe_load(stream)
+            config = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
 
 def write_sample_config():
+    global config
     config = dict()
     config["url"]="http://www.alximedia.de/radio/"
     config["feedfile"]="~/radio_darc_feed.rss"
@@ -35,19 +72,36 @@ def write_sample_config():
         #print(yaml.dump(config, default_flow_style=False))
 
 
+def CreateElements():
+    global config
+    global PodElements
+    #read the webside
+    print("Reading website %s" % (config["url"]))
+    try:
+        f = urllib.request.urlopen(config["url"])
+    except urllib.error.URLError as e:
+        if hasattr(e, "code"):
+            print("Opening the webside failed with the following code: %s" % (e.code))
+        if hasattr(e,"reason"):
+            print("Error reason: %s" % (e.reason))
+        sys.exit(2)
 
+    website = str(f.read())
+    f.close()
 
-
-
-
-
+    for line in str(website).split("<br>"):
+        if str(line).find(".mp3\">RADIO DARC") != -1:
+            PodElements.append(PodElement(line))
 
 
 def main():
-    config = read_config()
+    read_config()
+    CreateElements()
+
+    for elem in PodElements:
+        elem.ExtractData()
 
 
 
 if __name__ == '__main__':
-    #main()
-    read_config()
+    main()
